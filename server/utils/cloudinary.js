becbe1bbbed08f0001800258ed55fa09b831f0cb
Ashwani Kumar <ashwanijopin5@gmail.com>
2026-06-11 01:12:01 +0530

    cloudinary probelm

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,9 +1,9 @@
 import {v2 as cloudinary} from "cloudinary"
 import dotenv from "dotenv"
-console.log("cludinary ",process.env.CLOUD_NAME);
+console.log("cludinary ",process.env.ClOUD_NAME);
 dotenv.config()
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
+    cloud_name:process.env.ClOUD_NAME,
     api_key:process.env.API_KEY,
     api_secret:process.env.API_SECRET
 })
