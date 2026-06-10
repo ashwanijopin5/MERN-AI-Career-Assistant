602fa783a34d6e8f25f534153549b781e056d886
Ashwani Kumar <ashwanijopin5@gmail.com>
2026-06-10 17:52:56 +0530

    Configure production API URLs

--- client/src/utils/constent.js
+++ client/src/utils/constent.js
@@ -1,5 +1,7 @@
-export const USER_END_POINT="/user"
-export const job_END_POINT="/job"
-export const ANALYSIS_POINT="/analysis"
-export const RESUME_END_POINT="/resume"
-export const INTERVIEW_END_POINT="/interview"
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const USER_END_POINT = `${API_URL}/user`;
+export const job_END_POINT = `${API_URL}/job`;
+export const ANALYSIS_POINT = `${API_URL}/analysis`;
+export const RESUME_END_POINT = `${API_URL}/resume`;
+export const INTERVIEW_END_POINT = `${API_URL}/interview`;
