9d4cfaea3897001f4f0f86568f8f174fe7b18fd7
Ashwani Kumar <ashwanijopin5@gmail.com>
2026-06-10 23:45:02 +0530

    Fix auth cookie for production

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -104,7 +104,7 @@ export const login = async (req, res) => {
         }
 
         return res.status(200).cookie("token", token, {
-            httpOnly: true, sameSite: "lax", maxAge: 1 * 24 * 60 * 60 * 1000
+            httpOnly: true, secure: true,sameSite: "none", maxAge: 1 * 24 * 60 * 60 * 1000
         }).json({
             success: true,
             message: `welcome back ${user.name}`,
@@ -124,7 +124,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+        return res.status(200).cookie("token", "", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 0
+        }).json({
             success: true,
             message: "logout succesfully"
         })
